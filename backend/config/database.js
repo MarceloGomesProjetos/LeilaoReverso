98270d8347e5d6c5e85b336db68a7d23a8a476a6
Marcelo Gomes <web.marcelo@gmail.com>
2026-01-20 15:22:50 -0300

    Tentando corrigir erro no git

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,13 +1,10 @@
 // backend/config/database.js
 const { Pool } = require('pg');
-const dotenv = require('dotenv');
-
-dotenv.config();
 
 const pool = new Pool({
   host: process.env.DB_HOST || 'localhost',
   port: Number(process.env.DB_PORT) || 5432,
-  database: process.env.DB_NAME || 'LeilaoReverso',
+  database: process.env.DB_NAME || 'ShortWin',
   user: process.env.DB_USER || 'postgres',
   password: process.env.DB_PASSWORD,
 });
